256c9c4c56f55c28df898c25d3bc40d1a16b52db
tsunosin-source <tsunosin@gmail.com>
2026-04-07 12:22:46 +0900

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="min-h-screen bg-gray-100">
+        {children}
+      </body>
     </html>
   );
 }
